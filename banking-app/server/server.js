5891020c3b2cbd41d55fcf18976b03dbbb2bd348
StayinNasty <keganswensen@gmail.com>
2017-09-07 12:10:41 -0600

    Got auth0 working

--- banking-app/server/server.js
+++ banking-app/server/server.js
@@ -22,7 +22,42 @@ massive(process.env.CONNECTIONSTRING).then(db => {
     app.set('db', db);
 });
 
+passport.use(new Auth0Strategy({
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENT_ID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    callbackURL: process.env.AUTH_CALLBACK
+},
+    function (accessToken, refreshToken, extraParams, profile, done) {
+        const db = app.get('db');
+        db.find_user(profile.id).then(user => {
+            if(user[0]){
+                return done(null, user);
+            } else {
+                db.create_user([profile.displayName, profile.emails[0].value, profile.picture, profile.id]).then(user => {
+                    return done(null, user[0]);
+                });
+            }
+        })
+    }
+));
 
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
 
-const port = 3001;
+passport.deserializeUser(function(user, done) {
+    app.get('db').find_session_user(user[0].id).then(user => {
+        return done(null, user[0]);
+    });
+});
+
+app.get('/auth', passport.authenticate('auth0'));
+
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3001/#/private', 
+    failureRedirect: 'http://localhost:3001/#/'
+}))
+
+const port = 3010;
 app.listen(port, console.log(`It's lit on ${port} fam!`));
